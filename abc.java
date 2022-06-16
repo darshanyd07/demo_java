efbf44febcb61ba11c3c6d8b8702f7a4c1b33674
champ09D <darshanmhatre709@gmail.com>
2022-06-16 12:02:02 +0530

    Done Array File

---
+++ abc.java
@@ -0,0 +1,21 @@
+class abc
+{
+ public static void main(String dm[])
+ {
+ try
+{
+ while(true) 
+{
+System.out.println("Hello Git");
+Thread.sleep(60000);
+}
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+}
+
+}
+
+
